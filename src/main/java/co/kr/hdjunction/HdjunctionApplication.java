fd66a2e9984bcc26ea548033e6ada5199da0cd49
zidol <ajh3166@naver.com>
2022-06-21 15:18:58 +0900

    프로젝트 초기 세팅 커밋

---
+++ src/main/java/co/kr/hdjunction/HdjunctionApplication.java
@@ -0,0 +1,13 @@
+package co.kr.hdjunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdjunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdjunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/hdjunction/HdjunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.hdjunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdjunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
